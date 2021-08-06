ab48d2bb1aba5d20b6fc35192f7613d4335360ff
Deepanshu Chaudhary <85361211+chaudharydeepanshu@users.noreply.github.com>
2021-08-06 15:20:47 +0530

    changed google_mobile_ads to a new enhanced github repo

--- lib/ad_state.dart
+++ lib/ad_state.dart
@@ -6,7 +6,8 @@ class AdState {
   AdState(this.initialization);
 
   String get bannerAdUnitId => Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/6300978111'
+      // ? 'ca-app-pub-4350200185776333/3077024944' //real
+      ? 'ca-app-pub-3940256099942544/6300978111' //test
       : 'ca-app-pub-3940256099942544/2934735716';
 
   AdManagerBannerAdListener get adListener => _adListener;
