adbeaebdf238359c7ca1174dcc305fa64c1a1984
chaudharydeepanshu <deepanshuc2141@outlook.com>
2021-08-20 22:21:08 +0530

    version: 1.0.0+37 to 1.0.0+38 and updated dependencies

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,13 @@
 
 #include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
 #include <native_pdf_renderer/native_pdf_renderer_plugin.h>
-#include <url_launcher_windows/url_launcher_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
   NativePdfRendererPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("NativePdfRendererPlugin"));
-  UrlLauncherPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
